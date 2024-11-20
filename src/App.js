62bfccd6beeea84ab6ba902d1375ccf727ee64c7
Özkan Öner <ozkan@ozkanoner.com>
2024-11-20 18:09:00 +0300

    tailwind setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import './index.css'
+
 function App() {
   return (
     <div>
-      <h3>{process.env.NODE_ENV}</h3>
+      <h1 className="text-3xl font-bold underline bg-teal-500 p-4 text-center text-sky-100">
+        ENV: {process.env.NODE_ENV}
+      </h1>
     </div>
   );
 }

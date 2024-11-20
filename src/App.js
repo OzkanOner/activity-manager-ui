fff3a8d5d356a0b2279742ee1784ed2197cb21cb
Özkan Öner <ozkan@ozkanoner.com>
2024-11-20 18:15:46 +0300

    feat: bootstrap installed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import './index.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import { Button } from 'react-bootstrap';
+import './index.css';
 
 function App() {
   return (
@@ -6,6 +8,17 @@ function App() {
       <h1 className="text-3xl font-bold underline bg-teal-500 p-4 text-center text-sky-100">
         ENV: {process.env.NODE_ENV}
       </h1>
+      <div className="p-4 my-4 bg-slate-300 flex items-center justify-evenly">
+        <Button variant="primary">Primary</Button>
+        <Button variant="secondary">Secondary</Button>
+        <Button variant="success">Success</Button>
+        <Button variant="warning">Warning</Button>
+        <Button variant="danger">Danger</Button>
+        <Button variant="info">Info</Button>
+        <Button variant="light">Light</Button>
+        <Button variant="dark">Dark</Button>
+        <Button variant="link">Link</Button>
+      </div>
     </div>
   );
 }
